e19d9be53dc209df7eb1ebc190eeecd14f5215c7
owc-android <irfan.ansari@oneway.cab>
2018-03-13 17:46:09 +0530

    event commit

--- onewayuser/src/main/java/emazify/oneway/com/onewayuser/onewayuser.java
+++ onewayuser/src/main/java/emazify/oneway/com/onewayuser/onewayuser.java
@@ -1,8 +1,24 @@
 package emazify.oneway.com.onewayuser;
 
+import android.content.Context;
+import android.widget.Toast;
+
 /**
  * Created by owc-android on 13/3/18.
  */
 
 public class onewayuser {
+
+    private static onewayuser ourInstance = new onewayuser();
+
+    public static onewayuser getInstance() {
+        return ourInstance;
+    }
+
+    private onewayuser() {
+    }
+
+    public void makeMeAwesome(Context context, String data) {
+        Toast.makeText(context, "Awesome " + data, Toast.LENGTH_LONG).show();
+    }
 }
