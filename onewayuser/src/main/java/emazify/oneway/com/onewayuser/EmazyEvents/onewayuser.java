6b9954763fc1112092727ce9cb47a713e814bb52
owc-android <irfan.ansari@oneway.cab>
2018-03-13 17:16:43 +0530

    event commit

---
+++ onewayuser/src/main/java/emazify/oneway/com/onewayuser/EmazyEvents/onewayuser.java
@@ -0,0 +1,17 @@
+package emazify.oneway.com.onewayuser.EmazyEvents;
+
+/**
+ * Created by owc-android on 13/3/18.
+ */
+
+public class onewayuser {
+    private static onewayuser ourInstance = new onewayuser();
+
+    public static onewayuser getInstance() {
+        return ourInstance;
+    }
+
+    private onewayuser() {
+    }
+
+}
