8490025b899d2777481ce5961aa323a104a2390d
owc-android <irfan.ansari@oneway.cab>
2018-04-18 12:24:57 +0530

    Notification event update

--- app/src/main/java/com/oneway/emazify/MainActivity.java
+++ app/src/main/java/com/oneway/emazify/MainActivity.java
@@ -4,22 +4,10 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.emazify.EmazyInitialize;
-import com.loopj.android.http.JsonHttpResponseHandler;
-
-import org.apache.http.Header;
-import org.json.JSONObject;
-
-import Utils.ConnectionDetector;
-import Utils.Const;
-import Utils.Pref;
-import Utils.UserFunctions;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private ConnectionDetector mConnectionDetector;
-    private UserFunctions mUserFunctions;
     private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
