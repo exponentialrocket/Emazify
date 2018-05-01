bb4c3b343d13e47f42b60f2641f08532e9a198c9
owc-android <irfan.ansari@oneway.cab>
2018-05-01 12:41:49 +0530

    Notification event update

--- emazify/src/main/java/Utils/UserFunctions.java
+++ emazify/src/main/java/Utils/UserFunctions.java
@@ -416,10 +416,11 @@ public class UserFunctions{
 
             showErrorLog("emazifyAutoSystemUserProperty Url " + EMAZIFY_USER_AUTO_PROPERTY_URL);
             showErrorLog("emazifyAutoSystemUserProperty Params " + jsonParams.toString());
-
+            showErrorLog("emazifyAutoSystemUserProperty Key " + EMAZIFY_USER_AUTO_PROPERTY_KEY);
             StringEntity entity = new StringEntity(jsonParams.toString());
             entity.setContentType(new BasicHeader(HTTP.CONTENT_TYPE, CONTENT_TYPE));
             myAsyncHttpClient.addHeader("x-api-key", EMAZIFY_USER_AUTO_PROPERTY_KEY);
+
             myAsyncHttpClient.post(mContext, EMAZIFY_USER_AUTO_PROPERTY_URL, entity, CONTENT_TYPE, responseHandler);
         } catch (JSONException | UnsupportedEncodingException e) {
             e.printStackTrace();
