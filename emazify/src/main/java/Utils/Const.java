557b94aa542712aab2af7b9d5b3dd66865a63af5
owc-android <irfan.ansari@oneway.cab>
2018-05-12 15:19:30 +0530

    Notification event update

--- emazify/src/main/java/Utils/Const.java
+++ emazify/src/main/java/Utils/Const.java
@@ -2,7 +2,6 @@ package Utils;
 
 import android.annotation.SuppressLint;
 
-import com.emazify.BuildConfig;
 
 @SuppressLint("SdCardPath")
 public class Const {
@@ -11,7 +10,7 @@ public class Const {
     public static String CID;
     public static String PREF_FILE = "Emazify_Pref_File";
     public static final String PREF_EmazyCID = "emaziCID";
-    public static final String SDK_Version = "2.37";
+    public static final String SDK_Version = "2.38";
     public static final String GCM_TOKEN = "gcmToken";
     public static final String MESSAGE = "message";
     // public static boolean isLive = BuildConfig.isLive;
