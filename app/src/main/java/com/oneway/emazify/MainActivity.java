8ed54a9658497dd6fcbe5615de79a82520fd1511
owc-android <irfan.ansari@oneway.cab>
2018-05-30 15:05:59 +0530

    Notification event update- Staging Account Id added

--- app/src/main/java/com/oneway/emazify/MainActivity.java
+++ app/src/main/java/com/oneway/emazify/MainActivity.java
@@ -4,9 +4,6 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.emazify.EmazyInitialize;
-import com.google.firebase.messaging.RemoteMessage;
-
 
 public class MainActivity extends AppCompatActivity {
 
