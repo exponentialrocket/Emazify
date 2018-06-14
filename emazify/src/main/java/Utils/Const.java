62464886f472cdfa274b44d28d01f59507eb47f0
owc-android <irfan.ansari@oneway.cab>
2018-06-14 11:03:41 +0530

    Notification event update- Staging Account Id added

--- emazify/src/main/java/Utils/Const.java
+++ emazify/src/main/java/Utils/Const.java
@@ -6,7 +6,7 @@ import android.annotation.SuppressLint;
 @SuppressLint("SdCardPath")
 public class Const {
 
-    public static final String SDK_Version = "2.64";
+    public static final String SDK_Version = "2.65";
 
     public static String CID;
     public static String PREF_FILE = "Emazify_Pref_File";
