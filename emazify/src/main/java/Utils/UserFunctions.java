5b49970f22e7d8f35bd43e26fb668e2bbdf70c7b
owc-android <irfan.ansari@oneway.cab>
2018-08-20 12:28:05 +0530

    Notification event update- Staging Account Id added

--- emazify/src/main/java/Utils/UserFunctions.java
+++ emazify/src/main/java/Utils/UserFunctions.java
@@ -80,9 +80,9 @@ public class UserFunctions{
 /*
         if(!isLive){
 */
-            EMAZIFY_LOGIN_URL = "https://api.emazify.in/V1/androidLogin";
+           /* EMAZIFY_LOGIN_URL = "https://stagingapi.emazify.in/V1/androidLogin";
             EMAZIFY_USER_PROPERTY_URL = "https://immrpob4v7.execute-api.ap-south-1.amazonaws.com/user_property";
-        EMAZIFY_USER_AUTO_PROPERTY_URL = "https://api.emazify.in/V1/userAutoSystemProperties";
+        EMAZIFY_USER_AUTO_PROPERTY_URL = "https://stagingapi.emazify.in/V1/userAutoSystemProperties";
 
            // EMAZIFY_USER_AUTO_PROPERTY_URL = "https://py3r76iv4j.execute-api.ap-south-1.amazonaws.com/user_auto_system_property";
             EMAZIFY_EVENTS = "https://ngi797uwx1.execute-api.ap-south-1.amazonaws.com/user_custom_event";
@@ -95,12 +95,12 @@ public class UserFunctions{
             EMAZIFY_USER_AUTO_PROPERTY_KEY = "w8KYpQLT0o3Wmc41qFMqcmFlsHLmrz4CvdfEps10";
             EMAZIFY_EVENTS_KEY = "fhuVU0192P6mCq3r4RHe86sqAOvEzwgoDwfJ7nhb";
             EMAZIFY_NOTI_UPDATE_KEY= "Zc9NxSUJ6d7kkerODyzvI8twTyGTrMWz9vpVYOES";
-            EMAZIFY_APP_DETECT_KEY= "EKVa1qoLNc47qTzAoDHmQ6itfa2Eqq1249LQY5dM";
+            EMAZIFY_APP_DETECT_KEY= "EKVa1qoLNc47qTzAoDHmQ6itfa2Eqq1249LQY5dM";*/
 
 
-          /*  EMAZIFY_LOGIN_URL = "https://m0vd9ivjpj.execute-api.ap-south-1.amazonaws.com/user_login_api_live";
+            EMAZIFY_LOGIN_URL = "https://stagingapi.emazify.in/V1/androidLogin";
             EMAZIFY_USER_PROPERTY_URL = "https://fkhmt0jdjj.execute-api.ap-south-1.amazonaws.com/user_property_live";
-            EMAZIFY_USER_AUTO_PROPERTY_URL = "https://jdypv37xbl.execute-api.ap-south-1.amazonaws.com/user_auto_system_property_live";
+            EMAZIFY_USER_AUTO_PROPERTY_URL = "https://stagingapi.emazify.in/V1/userAutoSystemProperties";
             EMAZIFY_EVENTS = "https://jyu38ju154.execute-api.ap-south-1.amazonaws.com/user_custom_event_live";
             EMAZIFY_NOTI_UPDATE_URL = "https://nl1sezi3n1.execute-api.ap-south-1.amazonaws.com/campaign_notification_event_update_live";
             EMAZIFY_APP_DETECT_URL = "https://7vt5cvw6r5.execute-api.ap-south-1.amazonaws.com/update_app_detected_live";
@@ -111,7 +111,7 @@ public class UserFunctions{
             EMAZIFY_USER_AUTO_PROPERTY_KEY = "rA5dTpW9xQ6wcGAfBRFjZN9l37Ptyk941ZTiLo26";
             EMAZIFY_EVENTS_KEY = "5dyLY8WlFReBpkk2mrpt12caNum8I2S18JKzkYp0";
             EMAZIFY_NOTI_UPDATE_KEY= "LJhteNWdIE8BvvYL8Geji6k6DiwAT1VSlNQqSeFj";
-            EMAZIFY_APP_DETECT_KEY= "xfPLSMavRR3409W4wkS9f6YHI9gdAlFB7QfH552H";*/
+            EMAZIFY_APP_DETECT_KEY= "xfPLSMavRR3409W4wkS9f6YHI9gdAlFB7QfH552H";
 
     }
 
